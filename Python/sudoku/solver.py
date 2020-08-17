31b2b245c75b84c35d6f94f5307d420b8bf859ce
TealKoi <bmbu2009@gmail.com>
2020-08-17 12:21:18 -0500

    Comments added to solver. Need to review sudoku.py

--- Python/sudoku/solver.py
+++ Python/sudoku/solver.py
@@ -1,16 +1,6 @@
+# solver.py
 
-puzzle = [
-    [5,3,0,0,7,0,0,0,0],
-    [6,0,0,1,9,5,0,0,0],
-    [0,9,8,0,0,0,0,6,0],
-    [8,0,0,0,6,0,0,0,3],
-    [4,0,0,8,0,3,0,0,1],
-    [7,0,0,0,2,0,0,0,6],
-    [0,6,0,0,0,0,2,8,0],
-    [0,0,0,4,1,9,0,0,5],
-    [0,0,0,0,8,0,0,7,9],
-]
-
+# Print the current board layout
 def print_board(board):
     for r in range(len(board)):
         if r % 3 == 0 and r != 0:
@@ -23,6 +13,8 @@ def print_board(board):
             else:
                 print(str(board[r][c]) + " ", end="")
 
+# Find empty squares on the board (as noted by a 0)
+# returns co-ordinates of empty square or None if the puzzle is solved
 def find_empty(board):
     for r in range(len(board)):
         for c in range(len(board[r])):
@@ -30,10 +22,13 @@ def find_empty(board):
                 return (r,c)
     return None
 
+# Determine if the a given number is valid in a given position on the board
 def valid(board, num, pos):
+    # Evaluate Row
     for i in range(len(board[0])):
         if board[pos[0]][i] == num and pos[1] != i:
             return False
+    # Evaluate Column
     for i in range(len(board)):
         if board[i][pos[1]] == num and pos[0] != i:
             return False
@@ -41,14 +36,16 @@ def valid(board, num, pos):
     box_x = pos[1] // 3
     box_y = pos[0] // 3
 
+    # Evaluate 3x3 Box
     for i in range(box_y*3, box_y*3 + 3):
         for j in range(box_x*3, box_x*3 + 3):
             if board[i][j] == num and (i,j) != pos:
                 return False
     return True
 
+# Solve the board entierly
 def solve(board):
-    find = find_empty(puzzle)
+    find = find_empty(board)
     if not find:
         return True
     else:
@@ -62,6 +59,18 @@ def solve(board):
     return False
 
 def main():
+    puzzle = [
+        [5,3,0,0,7,0,0,0,0],
+        [6,0,0,1,9,5,0,0,0],
+        [0,9,8,0,0,0,0,6,0],
+        [8,0,0,0,6,0,0,0,3],
+        [4,0,0,8,0,3,0,0,1],
+        [7,0,0,0,2,0,0,0,6],
+        [0,6,0,0,0,0,2,8,0],
+        [0,0,0,4,1,9,0,0,5],
+        [0,0,0,0,8,0,0,7,9],
+    ]
+
     print_board(puzzle)
     solve(puzzle)
     print("   ")
